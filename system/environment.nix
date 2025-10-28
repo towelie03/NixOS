20619693e76dcf6a0963766fcdbc45ae3ad3d04c
towelie03 <dbalsara2003@gmail.com>
2025-10-27 23:58:19 -0700

    ;

--- system/environment.nix
+++ system/environment.nix
@@ -9,9 +9,9 @@
   environment.variables = {
     XCURSOR_SIZE = "24";
     QT_QPA_PLATFORM = "wayland";
-    ELECTRON_OZONE_PLATFORM_HINT "auto"
-    GDK_SCALE "1"
-    QT_QPA_PLATFORMTHEME "gtk3"
-    QT_QPA_PLATFORMTHEME_QT6 "gtk3"
+    ELECTRON_OZONE_PLATFORM_HINT "auto";
+    GDK_SCALE "1";
+    QT_QPA_PLATFORMTHEME "gtk3";
+    QT_QPA_PLATFORMTHEME_QT6 "gtk3";
   };
 }
