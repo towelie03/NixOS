fcfcdb8816335e5ca714f1abf0eba4b3a5a0bf5d
Daryush Balsara <dbalsara2003@gmail.com>
2025-10-28 18:38:51 +0000

    Add ghostty and alacritty to applications.nix

--- home/niri/applications.nix
+++ home/niri/applications.nix
@@ -1,6 +1,7 @@
 { pkgs }:
 {
   browser = "${pkgs.brave}/bin/brave";
-  terminal = "${pkgs.ghostty}/bin/ghostty";
+  ghostty = "${pkgs.ghostty}/bin/ghostty";
+  alacritty = "${pkgs.ghostty}/bin/alacritty";
   fileManager = "${pkgs.xfce.thunar}/bin/thunar";
 }
