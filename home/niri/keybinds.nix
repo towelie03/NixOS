33e2c881ab1104bf955bd16f615286657be30e66
Daryush Balsara <dbalsara2003@gmail.com>
2025-10-28 18:56:44 +0000

    Refactor keybindings for terminal and workspace actions

--- home/niri/keybinds.nix
+++ home/niri/keybinds.nix
@@ -10,8 +10,8 @@ in {
     "super+Shift+Slash".action = show-hotkey-overlay;
 
     # Terminals
-    "control+Return".action = spawn apps.ghostty;
-    "control+shift+Return".action = spawn apps.alacritty;
+    "Ctrl+Return".action = spawn apps.ghostty;
+    "Ctrl+shift+Return".action = spawn apps.alacritty;
 
     # Apps
     "super+b".action = spawn apps.browser;
@@ -31,16 +31,6 @@ in {
     "super+7".action = focus-workspace 7;
     "super+8".action = focus-workspace 8;
     "super+9".action = focus-workspace 9;
-
-    "super+shift+1".action = move-column-to-workspace 1;
-    "super+shift+2".action = move-column-to-workspace 2;
-    "super+shift+3".action = move-column-to-workspace 3;
-    "super+shift+4".action = move-column-to-workspace 4;
-    "super+shift+5".action = move-column-to-workspace 5;
-    "super+shift+6".action = move-column-to-workspace 6;
-    "super+shift+7".action = move-column-to-workspace 7;
-    "super+shift+8".action = move-column-to-workspace 8;
-    "super+shift+9".action = move-column-to-workspace 9;
 
     # Column focus/movement
     "super+j".action = focus-column-left;
@@ -72,7 +62,7 @@ in {
 
     # Screenshots
     "super+shift+print".action = screenshot;
-    #"control+print".action = screenshot-screen;
+    #"Ctrl+print".action = screenshot-screen;
     #"alt+print".action = screenshot-window; # optional
   };
 }
